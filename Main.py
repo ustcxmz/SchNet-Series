353a1ba902912ce0cf71b2517537602e0605a56c
ustcxmz <3685758901@qq.com>
2025-04-08 18:18:28 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,13 @@ from SchNet_plus_on_water_molecule_dataset import train as SchNet_plus_train
 from SchNet_plus_plus_on_water_molecule_datset import train as SchNet_plus_plus_train
 
 if __name__ == "__main__":
+    print("SchNet Training")
     SchNet_train()
+    print("-"*20)
+    print("SchNet+ Training")
     SchNet_plus_train()
+    print("-"*20)
+    print("SchNet++ Training")
     SchNet_plus_plus_train()
+    print("-"*20)
     evaluate()
